4cf5141c281b2ed89731e3ac3a45c9b2938cb72f
Abhinav Madahar <abhinavmadahar@gmail.com>
2021-06-22 12:10:37 -0400

    Install tslib
    Our Typescript configuration requires tslib, which was not installed.
    This threw an error. I fixed it by installing tslib in the project's
    root directory. Keep in mind that installing tslib in the reactApp
    directory does not work; it must be installed in the root directory.

--- reactApp/src/footer.test.tsx
+++ reactApp/src/footer.test.tsx
@@ -1,5 +1,5 @@
 // __tests__/fetch.test.js
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Footer from './footer';
