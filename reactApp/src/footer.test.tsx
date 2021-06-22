62f0d210fb272c746737dbcf04a892c66772ff19
Abhinav Madahar <abhinavmadahar@gmail.com>
2021-06-22 13:04:00 -0400

    Manage tests using Jest
    Prior to this commit, we already used testing-library to run tests, but
    it was a bit messy to run them ourselves. This commit adds Jest to our
    codebase. Jest manages all our tests, and it integrates into VSCode.
    
    I set up Jest in the root directory, so it can test both our frontend
    and our backend. Right now, only the frontend has tests, but it will be
    easier this way to add tests to the backend. Also, I added Typescript
    support to Jest. I also moved the tsconfig.json file to the root
    directory because Jest needs access to it there. This also means that
    the backend will support TS.

--- reactApp/src/footer.test.tsx
+++ reactApp/src/footer.test.tsx
@@ -1,11 +1,12 @@
 // __tests__/fetch.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Footer from './footer';
 
 test('loads and displays footer', async () => {
-  render(<Footer />)
+  render(<Footer />);
 
   expect(screen.getByAltText('LinkedIn'));
   expect(screen.getByAltText('GitHub'));
